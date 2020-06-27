954333f7cc7f47d1145441709809a47a898e1cbd
Baderik <baderikpavel@gmail.com>
2020-06-27 11:29:35 +1000

    Done base frontend; Created base templates; Created less and js fot templates;

---
+++ static/javascript/profile.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".fa-briefcase").on("click", function (event) {
+        addNotification("Это ещё не работает. А это значит, что скоро будет");
+    });
+    $(".fa-comments").on("click", function (event) {
+        addNotification("Это ещё не работает. А это значит, что скоро будет");
+    })
+})
