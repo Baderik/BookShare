9e3039023947e647012dff2b5c5a34cd78964d04
Baderik <baderikpavel@gmail.com>
2020-06-29 17:32:27 +1000

    Added data processing from auth form; And added response;

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views import View
 
 from authentication.forms import LoginForm, RegisterForm
+from authentication.models import User
 
 
 class IndexView(View):
@@ -15,8 +18,95 @@ class LoginView(View):
     def get(request):
         return render(request, "auth/login.html", {"form": LoginForm()})
 
+    @staticmethod
+    def post(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("/")
+
+        result = {"code": "500", "message": "Произошла ошибка"}
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+
+            user = authenticate(email=email, password=password)
+
+            if user is not None:
+                if user.is_active_email:
+                    login(request, user)
+
+                    url = "/people/settings/"
+
+                    if user.is_active:
+                        url = "/post"
+
+                    result["code"] = "303"
+                    result["message"] = "Вам туда"
+                    result["location"] = url
+
+                else:
+                    result["code"] = "401"
+                    result["message"] = "Сначала подтвердите почту"
+
+            else:
+                result["code"] = "401"
+                result["message"] = "Пользователь c такой почтой" \
+                                    " и паролем не найден"
+
+        else:
+            result["code"] = "400"
+            result["message"] = "Проверьте правильно ли вы заполнили поля"
+            for field in ("email", "password"):
+                result[field] = "1" \
+                    if form.cleaned_data.get(field, "") \
+                    else ""
+
+        return JsonResponse(result)
+
 
 class SignUpView(View):
     @staticmethod
     def get(request):
         return render(request, "auth/register.html", {"form": RegisterForm()})
+
+    @staticmethod
+    def post(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("/")
+
+        result = {"code": "500", "message": "Произошла ошибка"}
+
+        form = RegisterForm(request.POST)
+
+        if not form.is_valid():
+            result["code"] = "400"
+            result["message"] = "Проверьте правильно ли вы заполнили поля"
+
+            for field in ("email",):
+                result[field] = "1" \
+                    if form.cleaned_data.get(field, "") \
+                    else ""
+
+            return result
+
+        email = form.cleaned_data.get("email", "")
+        user = User.objects.get(email=email)
+
+        if user is not None:
+            result["code"] = "400"
+            result["message"] = "Пользователь с такой почтой уже существует"
+
+        else:
+            user = User(email=email)
+
+            password = User.objects.make_random_password()
+            user.set_password(password)
+            user.save()
+
+            print(f"Password: {password}")
+
+            result["code"] = "200"
+            result["message"] = "На вашу почту отправленно сообщение с инстукцией."
+
+        return JsonResponse(result)
