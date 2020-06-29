6345d73aae982b12fe780d71f5ee83670f8686f0
Baderik <baderikpavel@gmail.com>
2020-06-29 15:03:35 +1000

    Created model Profile; Model with onoToOne User;

--- people/models.py
+++ people/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from authentication.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    surname = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    middle_name = models.CharField(max_length=64)
+    phone = models.CharField(max_length=11, null=True, default=None)
+    image = models.IntegerField(default=0)
+
+
+@receiver(post_save, sender=User)
+def save_or_create_profile(sender, instance, created):
+    if created:
+        Profile.objects.create(user=instance)
+
+    else:
+        try:
+            instance.profile.save()
+
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
