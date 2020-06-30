303c8ea36d66dc04b41a61b2989d425db5cd53cd
Baderik <baderikpavel@gmail.com>
2020-06-30 13:55:29 +1000

    Created new model Article; Migrated;

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from people.models import User
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tittle = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.PositiveIntegerField()
+    condition = models.CharField(
+        max_length=255,
+        choices=(
+            ("old", "Б/у"),
+            ("new", "Новый")), default="old")
+    price = models.PositiveIntegerField()
+    tags = models.CharField(max_length=255)
+    images = models.CharField(max_length=255)
+    avatar = models.PositiveIntegerField()
+    date = models.DateTimeField(default=timezone.now)
