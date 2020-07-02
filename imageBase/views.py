215a1dcaf9f2b14bfe747c668996357f5e5a56f7
Baderik <baderikpavel@gmail.com>
2020-07-02 14:08:14 +1000

    Created urls image; Created views; Added convert file to jpeg and compress 70; Created index page;

--- imageBase/views.py
+++ imageBase/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, redirect, get_object_or_404, render
+from django.views import View
+from django.http import JsonResponse
 
-# Create your views here.
+from imageBase.models import ImageModel
+from imageBase.forms import UploadImageForm
+
+from PIL import Image
+
+import os
+
+
+class UploadView(View):
+    @staticmethod
+    def get(request):
+        return render(request, "imageBase/index.html", {"form": UploadImageForm()})
+
+    @staticmethod
+    def post(request):
+        if not request.user.is_authenticated:
+            return redirect("/")
+
+        form = UploadImageForm(request.POST, request.FILES)
+
+        if not form.is_valid():
+            return HttpResponse(status=400)
+
+        image = form.save()
+
+        return JsonResponse({"id": image.id})
+
+
+class GetView(View):
+    @staticmethod
+    def get(request, iid):
+        image = get_object_or_404(ImageModel, pk=iid)
+
+        return redirect(image.image.url)
