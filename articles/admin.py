328598ac6774f9840745d66f95f011ee4c556bff
Baderik <baderikpavel@gmail.com>
2020-07-04 19:24:25 +1000

    Added Tag to admin panel;

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from articles.models import Article, Quote
+from articles.models import Article, Quote, Tag
 
 admin.site.register(Article)
 admin.site.register(Quote)
+admin.site.register(Tag)
